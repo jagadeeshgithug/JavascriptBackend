5871a6da635c45be90ed10ad399099af369a8763
kesava jagadeesh <skjagadeesh123@gmail.com>
2022-05-10 20:34:11 +0530

    added get API calls

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { application } = require("express");
 const express = require("express");
 
 const app = express();
@@ -6,6 +7,40 @@ const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/api/v1/instagram", (req, res) => {
+  const ojbect = {
+    username: "jagadeesh",
+    followers: 100,
+    follows: 200,
+    date: new Date(),
+  };
+  res.status(200).json(ojbect);
+});
+
+app.get("/api/v1/facebook", (req, res) => {
+  const ojbect = {
+    username: "jagadeeshnaidu",
+    followers: 200,
+    follows: 300,
+    date: new Date(),
+  };
+  res.status(200).json(ojbect);
+});
+
+app.get("/api/v1/linkedIn", (req, res) => {
+  const ojbect = {
+    username: "kesavajagadeesh",
+    followers: 50,
+    follows: 500,
+    date: new Date(),
+  };
+  res.status(200).json(ojbect);
+});
+
+app.get("/api/v1/:token", (req, res) => {
+  res.send(req.params.token);
 });
 
 app.listen(port, () => {
