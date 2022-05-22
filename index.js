70e9cfb9dbfa8ac6968f9e6982abcafc69871c29
kesava jagadeesh <skjagadeesh123@gmail.com>
2022-05-22 15:21:10 +0530

    setup swagger file

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const express = require("express");
 
 const app = express();
 
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+
 const port = process.env.PORT || 3000;
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
